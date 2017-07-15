dea98cf35d9fe467909ae076b10dae17ff65fcf1
bradley sevison <bradleysev@gmail.com>
2017-07-15 17:29:03 -0600

    framework done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,35 @@
+var questionBtn;
+var i;
+var currentIndex;
+var currentAnswer;
+var currentQuestion;
+var currentQuestionList;
 var questions = [{
-            question: 'How many rings of power were given to man?',
-            correctAnswer: '9',
-            answers: ['5', '6', '3', '9'],
-            gif: "../images/question1Gif.gif",
+        question: 'How many rings of power were given to man?',
+        correctAnswer: '9',
+        answers: ['5', '6', '3', '9'],
+        gif: '../images/question1Gif.gif',
     },
 
     {
-            question: 'Who is the true king of Gondor?',
-            correctAnswer: 'Aragorn',
-            answers: ['Samwise Gamgee ', 'Boromir', 'Aragorn', 'Legolas'],
-            gif: "../images/question2Gif.gif",
+        question: 'Who is the true king of Gondor?',
+        correctAnswer: 'Aragorn',
+        answers: ['Samwise Gamgee ', 'Boromir', 'Aragorn', 'Legolas'],
+        gif: "../images/question2Gif.gif",
     },
 
     {
-            question: 'Who participated in the battle of isengard?',
-            correctAnswer: 'Saurumons Army',
-            answers: ['Saurumons Army | the Ents', 'Saurumons Army | King Theodens army', 'Orcs of Dol Gulder | the Galadhrim of Lothlorien.', 'Saurons Army | Gondors Army'],
-            gif: "../images/question1Gif.gif",
+        question: 'Who participated in the battle of isengard?',
+        correctAnswer: 'Saurumons Army | the Ents',
+        answers: ['Saurumons Army | the Ents', 'Saurumons Army | King Theodens army', 'Orcs of Dol Gulder | the Galadhrim of Lothlorien.', 'Saurons Army | Gondors Army'],
+        gif: "../images/question1Gif.gif",
     },
 
     {
-            question: 'How does farmer Maggot keep people off his farm, Bamfurlong?',
-            correctAnswer: 'A pack of dogs',
-            answers: ['Magical Barriers', 'A pack of dogs', 'A shape-shifiting man/bear', 'A swarm of angry bees'],
-            gif: "../images/question1Gif.gif",
+        question: 'How does farmer Maggot keep people off his farm, Bamfurlong?',
+        correctAnswer: 'A pack of dogs',
+        answers: ['Magical Barriers', 'A pack of dogs', 'A shape-shifiting man/bear', 'A swarm of angry bees'],
+        gif: "../images/question1Gif.gif",
     },
 
     {
@@ -46,20 +52,26 @@ var timeLimits = {
     switchScreenTime: 3, 
 }
 
-var currentQuestionList;
-
 var int = function(){
     scoreCount.correct = 0;
     scoreCount.incorrect = 0;
     scoreCount.unanswered = 0;
-    currentQuestionList = questions[0].answers;
-    $('#questionScreen').hide()
-    $('#endScreen').hide()
+    currentIndex = 0;
+    currentAnswer = questions[currentIndex].correctAnswer;
+    currentQuestion = questions[currentIndex].question;
+    currentQuestionList = questions[currentIndex].answers;
+    $('#questionScreen').hide();
+    $('#correctScreen').hide();
+    $('#incorrectScreen').hide();
+    $('#unansweredScreen').hide();
+    $('#endScreen').hide();
 }
 
+var intQuestion = function(){
+    $('#question').html(questions[currentIndex].question);
+};
 
-
-var intQuestions = function(){
+var intQuestionList = function(){
     for (i = 0; i < currentQuestionList.length; i++){
         var questionBtn = $('<button>');
         questionBtn.addClass('questionBtn block');
@@ -67,12 +79,8 @@ var intQuestions = function(){
         questionBtn.text(currentQuestionList[i]);
         $("#questionList").append(questionBtn);
     };
-    
 }
 
-var generateAnswerList = function(){
-
-}
 
 
 $(document).ready(function () {
@@ -81,7 +89,65 @@ $(document).ready(function () {
     $('#startBtn').click(function () {
         $('#startScreen').hide();
         $('#questionScreen').show();
-        intQuestions();
+        intQuestion();
+        intQuestionList();
     });
 
+    $('#questionScreen').on('click', '.questionBtn', function(event){
+       if($(this).data('answer') == currentAnswer) {
+            console.log('correct!')
+            $('#questionScreen').hide()
+            $('#correctScreen').show()
+            $('#questionList').empty();
+            $('#correctGif').attr('src', questions[currentIndex].gif)
+            scoreCount.correct++;
+            currentIndex++;
+            currentAnswer = questions[currentIndex].correctAnswer;
+            currentQuestion = questions[currentIndex].question;
+            currentQuestionList = questions[currentIndex].answers;
+        } else {
+            console.log('incorrect!')
+            $('#questionScreen').hide()
+            $('#incorrectScreen').show()
+            $('#questionList').empty();
+            $('#incorrectGif').attr('src', questions[currentIndex].gif)
+            scoreCount.incorrect++;
+            currentIndex++;
+            currentQuestionList = questions[currentIndex].answers;
+            currentAnswer = questions[currentIndex].correctAnswer;
+            currentQuestion = questions[currentIndex].question;
+            currentQuestionList = questions[currentIndex].answers;
+        }
+    });
+
+    $('#correctScreen, #incorrectScreen, #unansweredScreen').on('click', function(){
+        if (currentIndex === questions.length) {
+           $('#correctScreen, #incorrectScreen, #unansweredScreen').hide(); 
+           $('#endScreen, #scoreDisplay').show();
+           $('#scoreCorrect').html(scoreCount.correct);
+           $('#scoreIncorrect').html(scoreCount.incorrect);
+           $('#scoreUnanswered').html(scoreCount.unanswered);
+        } else {
+            intQuestion();
+            intQuestionList();
+            $('#questionScreen').show();
+            $('#correctScreen, #incorrectScreen, #unansweredScreen').hide();
+        }
+    });
+    
+    $('#endScreen').on('click', '#restartBtn', function(){
+    scoreCount.correct = 0;
+    scoreCount.incorrect = 0;
+    scoreCount.unanswered = 0;
+    currentIndex = 0;
+    currentAnswer = questions[currentIndex].correctAnswer;
+    currentQuestion = questions[currentIndex].question;
+    currentQuestionList = questions[currentIndex].answers;
+    $('#questionScreen').hide();
+    $('#correctScreen').hide();
+    $('#incorrectScreen').hide();
+    $('#unansweredScreen').hide();
+    $('#endScreen').hide();
+    $('#startScreen').show();
+    })
 });
